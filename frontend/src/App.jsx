7424ef01e664c3a2a377970a0886f2b9d800f8cd
kasturi2903 <pawarkasturi2906@gmail.com>
2024-12-23 14:48:49 +0530

    vite replaced

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React Bootstrap!</h1>
+      <Button variant="primary">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
